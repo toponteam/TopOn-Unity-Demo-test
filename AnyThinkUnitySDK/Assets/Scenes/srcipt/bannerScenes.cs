c7ecc90d040cedf1c4f3e4e6e4d954fa3cb06330
张健明 <253521624@qq.com>
2019-12-25 18:11:25 +0800

    bottom

--- AnyThinkUnitySDK/Assets/Scenes/srcipt/bannerScenes.cs
+++ AnyThinkUnitySDK/Assets/Scenes/srcipt/bannerScenes.cs
@@ -64,9 +64,9 @@ public class bannerScenes : MonoBehaviour {
         // ATRect arpuRect = new ATRect(0,50, this.screenWidth, 300, true);
         // ATBannerAd.Instance.showBannerAd(mPlacementId_native_all, arpuRect);
 
-        // ATBannerAd.Instance.showBannerAd(mPlacementId_native_all, ATBannerAdLoadingExtra.kATBannerAdShowingPisitionTop);
+        ATBannerAd.Instance.showBannerAd(mPlacementId_native_all, ATBannerAdLoadingExtra.kATBannerAdShowingPisitionBottom);
 
-        ATBannerAd.Instance.showBannerAd(mPlacementId_native_all, ATBannerAdLoadingExtra.kATBannerAdShowingPisitionTop);
+        // ATBannerAd.Instance.showBannerAd(mPlacementId_native_all, ATBannerAdLoadingExtra.kATBannerAdShowingPisitionTop);
     }
 
     public void removeBannerAd() {
