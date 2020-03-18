e56ad4afc2181feea501700e4858da31f6f58bc4
张健明 <253521624@qq.com>
2020-03-18 16:27:16 +0800

    add subchannel

--- AnyThinkUnitySDK/Assets/Scenes/srcipt/MainScenes.cs
+++ AnyThinkUnitySDK/Assets/Scenes/srcipt/MainScenes.cs
@@ -67,6 +67,7 @@ public class MainScenes : MonoBehaviour {
 		Debug.Log ("Developer init sdk");
 		Debug.Log(" Developer Screen size : {" + Screen.width + ", " + Screen.height + "}");
         ATSDKAPI.setChannel("unity3d_test_channel");
+        ATSDKAPI.setSubChannel("unity3d_test_Subchannel");
         ATSDKAPI.initCustomMap(new Dictionary<string, string> { { "unity3d_data", "test_data" } });
         ATSDKAPI.setCustomDataForPlacementID(new Dictionary<string, string> { { "unity3d_data_pl", "test_data_pl" } },"b5b728e7a08cd4");
         ATSDKAPI.setLogDebug(true);
